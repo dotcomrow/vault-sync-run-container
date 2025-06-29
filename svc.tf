29bf0fcad220dd4055f27933a54fc6d8793ee080
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-06-29 12:23:42 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- svc.tf
+++ svc.tf
@@ -3,6 +3,7 @@ resource "google_cloud_run_v2_service" "svc" {
   location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL"
   project  = "${var.project_name}-${random_id.suffix_gcp.hex}"
+  deletion_protection = false
 
   template {
     containers {
